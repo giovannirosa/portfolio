a60ea73276391981ec30986a11ee087a47e57521
Giovanni Rosa <giovanni.r@corvalent.com>
2025-06-30 02:12:16 -0300

    refactor: :green_heart: reduce api/chat edge function

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,8 +1,8 @@
 import { groq } from '@ai-sdk/groq';
 import { streamText } from 'ai';
-import wasm from "tiktoken/lite/tiktoken_bg.wasm?module";
-import model from "tiktoken/encoders/cl100k_base.json";
-import { init, Tiktoken } from "tiktoken/lite/init";
+// import wasm from "tiktoken/lite/tiktoken_bg.wasm?module";
+// import model from "tiktoken/encoders/cl100k_base.json";
+// import { init, Tiktoken } from "tiktoken/lite/init";
 import about from '@/data/about.json';
 import certifications from '@/data/certifications.json';
 import contact from '@/data/contact.json';
@@ -17,12 +17,12 @@ export const runtime = 'edge';
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
-  await init((imports) => WebAssembly.instantiate(wasm, imports));
-  const encoding = new Tiktoken(
-    model.bpe_ranks,
-    model.special_tokens,
-    model.pat_str
-  );
+  // await init((imports) => WebAssembly.instantiate(wasm, imports));
+  // const encoding = new Tiktoken(
+  //   model.bpe_ranks,
+  //   model.special_tokens,
+  //   model.pat_str
+  // );
   const { messages } = await req.json();
 
   let jsonBlock = JSON.stringify(about);
@@ -35,7 +35,7 @@ export async function POST(req: Request) {
   jsonBlock += JSON.stringify(projects);
   jsonBlock += JSON.stringify(skills);
 
-  console.log('tokens:', encoding.encode(jsonBlock).length);
+  // console.log('tokens:', encoding.encode(jsonBlock).length);
 
   // Prepend a system prompt to ground the model in your portfolio info:
   const system = {
